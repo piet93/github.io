49c960441585b6510fa98977f1db7f71ea7d7365
piet93 <61916935+piet93@users.noreply.github.com>
2020-03-26 16:09:05 +0100

    erste variabel test

--- javascript/hello.js
+++ javascript/hello.js
@@ -1,1 +1,15 @@
-alert("Hello world");
+// alert("Hello world");
+
+// let message = "Hello world";
+// alert(message);
+// message = 'Hallo Welt';
+// alert(message);
+
+const LINK_COLOR = '#FF0000'
+console.log("Link bitte in der Farbe", LINK_COLOR);
+
+let highscore = 523434;
+let fullname = "Jeffrey 'The Dude' Lebowski";
+fullname = 'Jeffrey "The Dude" Lebowski'; // geht beides
+console.log(fullname)
+
