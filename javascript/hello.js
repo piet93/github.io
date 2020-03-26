d3916614cdc0ed01fcd5d5d6334b5b861aea9186
piet93 <61916935+piet93@users.noreply.github.com>
2020-03-26 16:26:50 +0100

    listen_arrays

--- javascript/hello.js
+++ javascript/hello.js
@@ -13,4 +13,24 @@ let fullname = "Jeffrey 'The Dude' Lebowski";
 fullname = 'Jeffrey "The Dude" Lebowski'; // geht beides
 console.log(fullname)
 
+let template = `Dein Highscore sind ${highscore} Punkte`;
+console.log(template);
 
+
+// listen oder array
+let participants = ["John", "Peter", "Reto"];
+console.log(participants);
+console.log(participants.length);
+console.log(participants[0]);
+
+let user = {
+    Firstname: "John",
+    lastname: "Smith",
+    age: 25
+}
+
+console.log(user);
+console.log(user.Firstname);
+user.highscore = 200;
+user["highscore ever"] = 400
+console.log(user);
