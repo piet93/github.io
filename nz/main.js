62de1487f359169d5cc8e0d021fc9f670eaf2047
piet93 <61916935+piet93@users.noreply.github.com>
2020-04-02 16:03:04 +0200

    karte funktioniert

--- nz/main.js
+++ nz/main.js
@@ -1,29 +1,23 @@
-var mymap = L.map('map').setView([-43.880833, 169.040278], 13);
 
-L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
+let mapdiv = document.querySelector("#map");
 
-//Marker für River Safari und weiteres PopUp dazu:
-var markerRiverSafari = L.marker([-43.880833, 169.040278]).addTo(mymap);
-markerRiverSafari.bindPopup("<b>Haast River Safari!</b><br>Experience real New Zealand.").openPopup();
+let map = L.map("map", {
+    center: [
+        mapdiv.dataset.lat,
+        mapdiv.dataset.lng
+    ],
+    zoom: 11,
+    layers: [
+        L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+            maxZoom: 17,
+            attribution: `Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)`
+        })
+    ]
+});
 
-//Marker für Stadt mit PopUp:
-var markerStadt = L.marker([-43.880833, 169.040278]).addTo(mymap);
-markerStadt.bindPopup("<b>Haast!</b><br>a cosy and quite town to stay.").openPopup();
+let mrk = L.marker([
+    mapdiv.dataset.lat,
+    mapdiv.dataset.lng
+]).addTo(map);
 
-// Beim Klicken auf beliebigen Punkt auf der Karte werden entsprechende Koordinaten angezeigt:
-var popup = L.popup();
-
-function onMapClick(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("You clicked the map at " + e.latlng.toString())
-        .openOn(mymap);
-}
-mymap.on('click', onMapClick);
+mrk.bindPopup(mapdiv.dataset.title).openPopup();
