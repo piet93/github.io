32bb1224af0f091ec61b055ea4dea4638a196b3b
piet93 <61916935+piet93@users.noreply.github.com>
2020-04-02 12:35:40 +0200

    karte 2

--- nz/main.js
+++ nz/main.js
@@ -1,2 +1,6 @@
 //alert("Hallo Welt!")
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+}).addTo(mymap);
