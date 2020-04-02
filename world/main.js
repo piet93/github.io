ac821f7f58af8f07ea8fa49173d94a77fc8fb7c5
piet93 <61916935+piet93@users.noreply.github.com>
2020-04-02 18:14:14 +0200

    mehrer daten

--- world/main.js
+++ world/main.js
@@ -8,6 +8,8 @@ let map = L.map("map", {
     ]
 });
 
+let circleGroup = L.featureGroup().addTo(map);
+
 L.control.layers({
     "OpenTopoMap": startLayer,
     "OpenStreetMap.Mapnik": L.tileLayer.provider("OpenStreetMap.Mapnik"),
@@ -15,20 +17,33 @@ L.control.layers({
     "Stamen.Watercolor": L.tileLayer.provider("Stamen.Terrain"),
     "Esri.WorldImagery": L.tileLayer.provider("Esri.WorldImagery"),
     "MtbMap": L.tileLayer.provider("MtbMap"),
+}, {
+    "Thematische Darstellung": circleGroup
 }).addTo(map);
 
-L.marker([0, 0]).addTo(map);
+// L.marker([0, 0]).addTo(map);
 
 console.log(CONFIRMED)
 
-for (let i = 1; i < CONFIRMED.length; i++) {
-    const row = CONFIRMED[i];
-    // console.log(row[2],row[3]);
-    let lat = row[2];
-    let lng = row[3];
-    let val = row[row.length - 1];
-    let mrk = L.marker([lat, lng]).addTo(map);
-    mrk.bindPopup(`${row[0]} ${row[1]}: ${val}`);
+let drawCircles = function (data) {
+    for (let i = 1; i < data.length; i++) {
+        const row = data[i];
+        // console.log(row[2],row[3]);
+        let lat = row[2];
+        let lng = row[3];
+        let val = row[row.length - 1];
+        // let mrk = L.marker([lat, lng]).addTo(map);
+        // mrk.bindPopup(`${row[0]} ${row[1]}: ${val}`);
 
-    let circle = L.circleMarker([lat, lng]).addTo(map);
+        let s = 0.5;
+        let rad = Math.sqrt(val / Math.PI);
+        let circle = L.circleMarker([lat, lng], {
+            radius: rad
+        }).addTo(circleGroup);
+        circle.bindPopup(`${row[0]} ${row[1]}: ${val}`);    
+    }
 }
+
+drawCircles(CONFIRMED);
+drawCircles(RECOVERED);
+drawCircles(DEATHS);
