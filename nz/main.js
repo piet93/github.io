672e1b892b592bea14d8d71f6f51b0e6d1fda331
piet93 <61916935+piet93@users.noreply.github.com>
2020-04-02 12:38:32 +0200

    austausch let

--- nz/main.js
+++ nz/main.js
@@ -1,6 +1,21 @@
 //alert("Hallo Welt!")
-var mymap = L.map('map').setView([51.505, -0.09], 13);
+let mymap = L.map('map').setView([51.505, -0.09], 13);
 L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     maxZoom: 17,
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>tributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https:/ntopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 }).addTo(mymap);
+
+let marker = L.marker([51.5, -0.09]).addTo(mymap);
+
+let circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap);
+
+let polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(mymap);
