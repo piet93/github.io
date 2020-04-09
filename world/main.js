9240327e623678fe1bca295c772cd973844f2684
piet93 <61916935+piet93@users.noreply.github.com>
2020-04-09 16:10:10 +0200

    auswahl menue

--- world/main.js
+++ world/main.js
@@ -23,27 +23,43 @@ L.control.layers({
 
 // L.marker([0, 0]).addTo(map);
 
-console.log(CONFIRMED)
+let drawCircles = function () {
+    let data = CONFIRMED;
+    let header = CONFIRMED[0];
+    let index = header.length - 1;
+    let options = document.querySelector("#pulldown").options;
+    let value = options[options.selectedIndex].value;
+    let label = options[options.selectedIndex].text;
+    //console.log(value,label,options);
 
-let drawCircles = function (data) {
+    // Datum & Thema anzeigen anzeigen
+    document.querySelector("#datum").innerHTML = `am ${header[index]} - ${label}`;
+
+    //console.log(data);
     for (let i = 1; i < data.length; i++) {
-        const row = data[i];
-        // console.log(row[2],row[3]);
+        let row = data[i];
+        //console.log(row[2],row[3]);
+        let reg = `${row[0]} ${row[1]}`;
         let lat = row[2];
         let lng = row[3];
-        let val = row[row.length - 1];
-        // let mrk = L.marker([lat, lng]).addTo(map);
-        // mrk.bindPopup(`${row[0]} ${row[1]}: ${val}`);
+        let val = row[index];
+        //let mrk = L.marker([lat,lng]).addTo(map);
+        //mrk.bindPopup(`${reg}: ${val}`);
 
+        //A = r²*PI
+        //r² = A/PI
+        //r = WURZEL(A/PI)
         let s = 0.5;
-        let rad = Math.sqrt(val / Math.PI);
+        let r = Math.sqrt(val * s / Math.PI);
         let circle = L.circleMarker([lat, lng], {
-            radius: rad
+            radius: r
         }).addTo(circleGroup);
-        circle.bindPopup(`${row[0]} ${row[1]}: ${val}`);    
+        circle.bindPopup(`${reg}: ${val}`);
     }
-}
+};
 
-drawCircles(CONFIRMED);
-drawCircles(RECOVERED);
-drawCircles(DEATHS);
+document.querySelector("#pulldown").onchange = function() {
+    drawCircles();
+};
+
+drawCircles();
