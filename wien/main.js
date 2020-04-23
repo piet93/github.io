16f9a1e91f0eac1cd10b912e3fd06bac0ef09b8e
piet93 <61916935+piet93@users.noreply.github.com>
2020-04-23 11:14:11 +0200

    wien template

---
+++ wien/main.js
@@ -0,0 +1,138 @@
+let startLayer = L.tileLayer.provider("BasemapAT.grau");
+
+let map = L.map("map", {
+    center: [48.208333, 16.373056],
+    zoom: 12,
+    layers: [
+        startLayer
+    ]
+});
+
+let sightGroup = L.markerClusterGroup().addTo(map);
+let walkGroup = L.featureGroup().addTo(map);
+let heritageGroup = L.featureGroup().addTo(map);
+
+L.control.layers({
+    "BasemapAT.grau": startLayer,
+    "BasemapAT": L.tileLayer.provider("BasemapAT"),
+    "BasemapAT.highdpi": L.tileLayer.provider("BasemapAT.highdpi"),
+    "BasemapAT.terrain": L.tileLayer.provider("BasemapAT.terrain"),
+    "BasemapAT.surface": L.tileLayer.provider("BasemapAT.surface"),
+    "BasemapAT.orthofoto": L.tileLayer.provider("BasemapAT.orthofoto"),
+    "BasemapAT.overlay": L.tileLayer.provider("BasemapAT.overlay"),
+    "BasemapAT.orthofoto+overlay": L.layerGroup([
+        L.tileLayer.provider("BasemapAT.orthofoto"),
+        L.tileLayer.provider("BasemapAT.overlay")
+    ])
+}, {
+    "Stadtspaziergang (Punkte)": sightGroup,
+    "Wanderungen": walkGroup,
+    "Weltkulturerbe": heritageGroup
+}).addTo(map);
+
+let sightUrl = "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SPAZIERPUNKTOGD &srsName=EPSG:4326&outputFormat=json";
+
+let sights = L.geoJson.ajax(sightUrl, {
+    pointToLayer: function (point, latlng) {
+        let icon = L.icon({
+            iconUrl: 'icons/sights.svg',
+            iconSize: [32, 32]
+        });
+        let marker = L.marker(latlng, {
+            icon: icon
+        });
+        // console.log("Point", point);
+        marker.bindPopup(`<h3>${point.properties.NAME}</h3>
+        <p>Adresse: ${point.properties.ADRESSE}</p>
+        <p>Beschreibung: ${point.properties.BEMERKUNG}</p>
+        <p><a target="links" href="${point.properties.WEITERE_INF}">Link</a></p>
+        `);
+        return marker;
+    }
+});
+
+sights.on("data:loaded", function () {
+    sightGroup.addLayer(sights);
+    // console.log('data loaded!');
+    map.fitBounds(sightGroup.getBounds());
+});
+
+let wandern = "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:WANDERWEGEOGD&srsName=EPSG:4326&outputFormat=json";
+
+L.geoJson.ajax(wandern, {
+    filter: function (feature) {
+        // console.log("Feature in filter: ", feature);
+        return feature.properties.KATEGORIE == "StWW";
+    },
+    style: function () {
+        return {
+            color: "black",
+            weight: 3,
+            dashArray: "8,6"  
+        };
+    },
+    onEachFeature: function (feature, layer) {
+        // console.log("Feature: ", feature);
+        layer.bindPopup(`<h3>${feature.properties.BEZ_TEXT}</h3>
+        `);
+    }
+}).addTo(map);
+
+L.geoJson.ajax(wandern, {
+    filter: function (feature) {
+        // console.log("Feature in filter: ", feature);
+        return feature.properties.KATEGORIE == "rundumadum";
+    },
+    style: function () {
+        return {
+            color: "black",
+            weight: 3,
+            dashArray: "0,5"  
+        };
+    },
+    onEachFeature: function (feature, layer) {
+        // console.log("Feature: ", feature);
+        layer.bindPopup(`<h3>${feature.properties.BEZ_TEXT}</h3>
+        `);
+    }
+}).addTo(map);
+
+let heritage = "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:WELTKULTERBEOGD&srsName=EPSG:4326&outputFormat=json";
+
+L.geoJson.ajax(heritage, {
+    filter: function (feature) {
+        // console.log("Feature in filter: ", feature);
+        return feature.properties.TYP == 1;
+    },
+    style: function () {
+        return {
+            color: "red",
+            fillOpacity: 0.3
+        };
+    },
+    onEachFeature: function (feature, layer) {
+        // console.log("Feature: ", feature);
+        layer.bindPopup(`<h3>${feature.properties.NAME}</h3>
+        <p>${feature.properties.INFO}</p>
+        `);
+    }
+}).addTo(map);
+
+L.geoJson.ajax(heritage, {
+    filter: function (feature) {
+        // console.log("Feature in filter: ", feature);
+        return feature.properties.TYP == 2;
+    },
+    style: function () {
+        return {
+            color: "yellow",
+            fillOpacity: 0.3
+        };
+    },
+    onEachFeature: function (feature, layer) {
+        // console.log("Feature: ", feature);
+        layer.bindPopup(`<h3>${feature.properties.NAME}</h3>
+        <p>${feature.properties.INFO}</p>
+        `);
+    }
+}).addTo(map);
