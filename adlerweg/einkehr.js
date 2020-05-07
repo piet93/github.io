0c2eb584b2540a000a0f8981214d8e8be37d79f5
piet93 <61916935+piet93@users.noreply.github.com>
2020-05-07 12:01:56 +0200

    einkehr.js

---
+++ adlerweg/einkehr.js
@@ -0,0 +1,12 @@
+const EINKEHR = [
+    [ "Etappennummer (25)", "Stoanalm", "47.024944", "12.316395" ],
+    [ "Etappennummer (25)", "Essener- und Rostocker Hütte", "47.054859", "12.297774" ],
+    [ "Etappennummer (25)", "Johannishütte", "47.059877", "12.334337" ],
+    [ "Etappennummer (28)", "Neue Sajathütte", "47.037904", "12.353436" ],
+    [ "Etappennummer (29)", Einkehrmöglichkeit, Lat, Lng ],
+    [ "Etappennummer (30)", Einkehrmöglichkeit, Lat, Lng ],
+    [ "Etappennummer (31)", Einkehrmöglichkeit, Lat, Lng ],
+    [ "Etappennummer (32)", Einkehrmöglichkeit, Lat, Lng ],
+    [ "Etappennummer (33)", Einkehrmöglichkeit, Lat, Lng ],
+    // u.s.w.
+];
