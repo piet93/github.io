96af39e894d51c2d8d4c8c4230405aedee041784
piet93 <61916935+piet93@users.noreply.github.com>
2020-05-07 13:10:05 +0200

    einkehr 5

--- adlerweg/einkehr.js
+++ adlerweg/einkehr.js
@@ -8,9 +8,12 @@ const EINKEHR = [
     [ "Etappennummer (28)", "Badener Hütte", "47.081128", "47.081128" ],
     [ "Etappennummer (29)", "Venedigerhaus", "47.123304", "12.451420" ],
     [ "Etappennummer (29)", "Matreier Tauernhaus", "47.119115", "12.496004" ],
-    [ "Etappennummer (32)", "Bergeralm", "47,048350", "12,621782" ],
-    [ "Etappennummer (32)", "Moaalm", "47,032404", "12,628645" ],
-    [ "Etappennummer (32)", "Stüdlhütte", "47,054769", "12,680953" ],
+    [ "Etappennummer (29)", "Steiner Alm", "47.030466", "12.549125" ],
+    [ "Etappennummer (29)", "Sudetendeutsche Hütte", "47.049294", "12.576307" ],
+    [ "Etappennummer (31)", "Kalser Tauernhaus", "47.070630", "12.623780" ],
+    [ "Etappennummer (32)", "Bergeralm", "47.048350", "12.621782" ],
+    [ "Etappennummer (32)", "Moaalm", "47.032404", "12.628645" ],
+    [ "Etappennummer (32)", "Stüdlhütte", "47.054769", "12.680953" ],
     [ "Etappennummer (33)", "Glorer Hütte", "47.031113", "12.715605" ],
-    [ "Etappennummer (33)", "Luchknerhaus", "47,020814", "12,687880" ]
+    [ "Etappennummer (33)", "Luchknerhaus", "47.020814", "12.687880" ]
 ];
